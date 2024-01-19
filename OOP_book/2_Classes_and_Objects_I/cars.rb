2749cf126e7c3fddeb282e4b6c56d43b77b0a2bf
Sylvain Ni <sylvainni@msn.com>
2024-01-19 16:17:27 +0100

    Examples

---
+++ OOP_book/2_Classes_and_Objects_I/cars.rb
@@ -0,0 +1,64 @@
+# - Create a class called MyCar
+# - When you initialize a new instance or object of the class, allow the user to
+# define some instance variables that tell us the year, model, and color of the car.
+# - Create an instance variable that is set to 0 during instantiation of the object
+# to track the current speed of the car as well.
+# - Create instance methods that allow the ar to speed up, brake, and shut the car off.
+#
+
+
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed}"
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+puts lumina.color
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+lumina.current_speed
+
+lumina.color = 'black'
+puts lumina.color
+puts lumina.year
+
+lumina.spray_paint('red')
+puts lumina.color
---
+++ OOP_book/2_Classes_and_Objects_I/good_dog.rb
@@ -0,0 +1,50 @@
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  # attr_accessor :name is equivalent to:
+  # def name     => This was renamed from "get_name"
+  #   @name
+  #  end
+  #
+  # def name=(n) => This was renamed from "set_name="
+  #  @name = n
+  # end
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def speak
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
+
+  def some_method
+    self.info
+  end
+end
+
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
+
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
+
+# puts sparky.speak
+# puts sparky.name
+# sparky.name = 'Spartacus' # equivalent to sparky.set_name=("Spartacus")
+# puts sparky.name
+
+# fido = GoodDog.new('Fido')
+# puts fido.speak
