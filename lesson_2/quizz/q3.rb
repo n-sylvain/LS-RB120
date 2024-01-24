ed645e6683fad11dcd953692c78e8a49f82997dd
Sylvain Ni <sylvainni@msn.com>
2024-01-24 22:09:10 +0100

    Quizz code

---
+++ lesson_2/quizz/q3.rb
@@ -0,0 +1,9 @@
+class Student
+  # assume that this code includes an appropriate #initialize method
+end
+
+jon = Student.new('John', 22)
+jon.name # => 'John'
+jon.name = 'Jon'
+jon.grade = 'B'
+jon.grade # => 'B'
---
+++ lesson_2/quizz/q5.rb
@@ -0,0 +1,25 @@
+class Character
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} is speaking."
+  end
+end
+
+class Thief < Character
+
+  # def initialize(name)
+  #   @name = name + " is whispering..."
+  # end
+  def speak
+    "#{name} is whispering..."
+  end
+end
+
+sneak = Thief.new("Sneak")
+p sneak.name # => "Sneak"
+p sneak.speak # => "Sneak is whispering..."
