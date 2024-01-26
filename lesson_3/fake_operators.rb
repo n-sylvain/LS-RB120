bcb9198d343567dbee74ac8f20ab118a87155c4f
Sylvain Ni <sylvainni@msn.com>
2024-01-26 10:26:17 +0100

    Lesson 3 example

---
+++ lesson_3/fake_operators.rb
@@ -0,0 +1,70 @@
+class Team
+  attr_accessor :name, :members
+
+  def initialize(name)
+    @name = name
+    @members = []
+  end
+
+  def <<(person)
+    members.push person
+  end
+
+  def +(other_team)
+    temp_team = Team.new("Temporary Team")
+    temp_team.members = members + other_team.members
+    temp_team
+  end
+
+  def [](idx)
+    members[idx]
+  end
+
+  def []=(idx, obj)
+    members[idx] = obj
+  end
+end
+
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def >(other_person)
+    age > other_person.age
+  end
+end
+
+cowboys = Team.new("Dallas Cowboys")
+cowboys << Person.new("Troy Aikman", 48)
+cowboys << Person.new("Emmitt Smith", 46)
+cowboys << Person.new("Michael Irvin", 49)
+
+
+niners = Team.new("San Francisco 49ers")
+niners << Person.new("Joe Montana", 59)
+niners << Person.new("Jerry Rice", 52)
+niners << Person.new("Deion Sanders", 47)
+
+# p cowboys
+# p niners
+
+dream_team = cowboys + niners               # what is dream_team?
+
+p dream_team
+
+cowboys.members                           # => ... array of 3 Person objects
+
+p cowboys[1]                                # => #<Person:0x007fae9295d830 @name="Emmitt Smith", @age=46>
+cowboys[3] = Person.new("JJ", 72)
+p cowboys[3]
+
+dream_team.name = "Dream Team"
+
+dream_team[4]
+dream_team[5] = Person.new("JJ", 72)
+
+puts dream_team.inspect
