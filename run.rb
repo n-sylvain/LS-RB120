35e8c5121191d2cfe2a5f434840bfa5f61d5531f
Sylvain Ni <sylvainni@msn.com>
2024-02-14 12:19:43 +0100

    Code update

--- run.rb
+++ run.rb
@@ -1,64 +1,14 @@
-class FixedArray
-  def initialize(lmax_size)
-    @array = Array.new(max_size)
-  end
-
-  def [](index)
-    @array.fetch[index]
-  end
-
-  def to_a
-    @array.to_a
-  end
-
-  def []=(index, value)
-    self[index]
-    @array[index] = value
+class Student
+  def initialize(name, year)
+    @name = name
+    @year = year
   end
 end
 
+class Graduate
+  def initialize(name, year, parking); end
+end
 
-fixed_array = FixedArray.new(5)
-puts fixed_array[3] == nil
-puts fixed_array.to_a == [nil] * 5
-
-# fixed_array[3] = 'a'
-# puts fixed_array[3] == 'a'
-# puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
-
-# fixed_array[1] = 'b'
-# puts fixed_array[1] == 'b'
-# puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
-
-# fixed_array[1] = 'c'
-# puts fixed_array[1] == 'c'
-# puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
-
-# fixed_array[4] = 'd'
-# puts fixed_array[4] == 'd'
-# puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
-# puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
-
-# puts fixed_array[-1] == 'd'
-# puts fixed_array[-4] == 'c'
-
-# begin
-#   fixed_array[6]
-#   puts false
-# rescue IndexError
-#   puts true
-# end
-
-# begin
-#   fixed_array[-7] = 3
-#   puts false
-# rescue IndexError
-#   puts true
-# end
-
-# begin
-#   fixed_array[7] = 3
-#   puts false
-# rescue IndexError
-#   puts true
-# end
+class Undergraduate
+  def initialize(name, year); end
+end
